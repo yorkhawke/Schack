2a544b876668eba7f81bd69d10290d9d19db8158
yorkhawke <per_strom@live.se>
2015-05-11 22:13:28 +0200

    more meny

--- Schack/main.cpp
+++ Schack/main.cpp
@@ -16,7 +16,7 @@ int main()
 	Player p1;
 	Player p2;
 
-	Text text;
+	Text text,text1,text11,text2,text3;
 	Font font;
 	window.create(sf::VideoMode(SCRWIDTH, SCRHEIGHT), "Schack");
 
@@ -27,6 +27,7 @@ int main()
 	}
 	else
 	{
+		//Meny
 		text.setFont(font);
 		text.setString("Welcome\n	To\nSchack Simulator 3000\n---------Meny---------\n1.SetPlayers\n2.ShowScore\n3.PlayMatch\n4.Close");
 		text.setCharacterSize(40);
@@ -34,7 +35,21 @@ int main()
 		FloatRect textRect = text.getLocalBounds();
 		text.setOrigin(textRect.width / 2, textRect.height / 2);
 		text.setPosition(Vector2f(SCRWIDTH / 2.0f, SCRHEIGHT / 2.0f));
+
+		//choice 1
+		text1.setFont(font);
+		text11.setFont(font);
+		text1.setString("Please enter Player 1 name:");
+		text11.setString("Please enter Player 2 name");
+		text1.setCharacterSize(30);
+		text11.setCharacterSize(30);
+		text1.setOrigin(textRect.width / 2, textRect.height / 2);
+		text1.setPosition(Vector2f(SCRWIDTH / 2.0f, SCRHEIGHT / 2.0f));
+		text11.setOrigin(textRect.width / 2, textRect.height / 2);
+		text11.setPosition(Vector2f(SCRWIDTH / 2.0f, SCRHEIGHT / 2.0f));
+
 	}
+
 
 	while (window.isOpen())
 	{
@@ -43,6 +58,31 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Num1))
+		{
+			window.clear();
+			window.draw(text1);
+			window.display();
+			string P1name;
+			scanf("%s", P1name);
+			window.clear();
+			window.draw(text11);
+			window.display();
+			string P2name;
+			scanf("%s", P2name);
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num2))
+		{
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num3))
+		{
+
+		}
+		else if (Keyboard::isKeyPressed(Keyboard::Num4))
+		{
+			return 0;
 		}
 		window.clear();
 		window.draw(text);
