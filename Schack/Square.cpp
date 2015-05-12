9d00f38b59c41f2c8045e7d50c34a41c6a32f8fd
LinusPersson <linus-persson@live.se>
2015-05-12 20:28:36 +0200

    Update Square.cpp

--- Schack/Square.cpp
+++ Schack/Square.cpp
@@ -1,11 +1,40 @@
 #include "Square.h"
+#include <SFML\Graphics.hpp>
+using namespace sf;
 
 Square::Square()
 {
 
 }
-
 Square::~Square()
 {
 
+}
+Square::Square(Vector2f pos, Sprite sp, bool o)
+{
+	Position = pos;
+	sprite = sp;
+	if (o == true)
+	{
+		sprite.setColor(Color(0, 0, 255, 255));
+		o = false;
+	}
+	IsOccupied = o;
+	sprite.setPosition(pos);
+}
+Vector2f Square::GetPosition()
+{
+	return this->Position;
+}
+Sprite Square::GetTexture()
+{
+	return this->sprite;
+}
+bool Square::GetIsOccupied()
+{
+	return this->IsOccupied;
+}
+void Square::SetIsOccupied(bool o)
+{
+	IsOccupied = o;
 }
