8263bf2f353d329b7d9bba18c4e074aa1547e034
LinusPersson <linus-persson@live.se>
2015-05-12 20:19:18 +0200

    Update Bishop.h

--- Schack/Bishop.h
+++ Schack/Bishop.h
@@ -10,14 +10,6 @@ private:
 public:
 	Bishop(Vector2f, bool, Sprite, Color);
 	~Bishop();
-	Vector2f GetPosition();
-	void SetPosition(Vector2f);
-	bool GetState();
-	void SetState(bool);
-	Sprite GetTexture();
-	void SetTexture(Sprite);
-	Color GetColour();
-	void SetColour(Color);
 	Vector2f Move(Vector2f);
 };
 
