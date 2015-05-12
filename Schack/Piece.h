cce03656ce2e3844015f385ada382cc8b7192264
LinusPersson <linus-persson@live.se>
2015-05-12 20:23:45 +0200

    Update Piece.h

--- Schack/Piece.h
+++ Schack/Piece.h
@@ -11,10 +11,23 @@ protected:
 	bool State;
 	Sprite sprite;
 	Color Colour;
+	bool Targeted;
 	Piece();
 public:
+
 	Piece(Vector2f, bool, Sprite, Color);
 	virtual ~Piece();
+	Vector2f GetPosition();
+	void SetPosition(Vector2f);
+	bool GetState();
+	void SetState(bool);
+	Sprite GetTexture();
+	void SetTexture(Sprite);
+	Color GetColour();
+	void SetColour(Color);
+	virtual Vector2f Move(Vector2f)=0;
+	bool GetTargeted();
+	void SetTargeted(bool);
 };
 
 #endif
