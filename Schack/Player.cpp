dad9c4fe310128147c74f2afdacf759735bfd8d6
LinusPersson <linus-persson@live.se>
2015-05-13 15:09:01 +0200

    Update Player.cpp

--- Schack/Player.cpp
+++ Schack/Player.cpp
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include "PieceHeader.h"
+#include <SFML\Graphics.hpp>
 using namespace std;
 using namespace sf;
 Player::~Player()
@@ -16,22 +17,22 @@ Player::Player(int ID, int Nr, string name, bool c)
   this->NrOfWins=Nr;
   this->Name = name;
   this->Concede = c;
-  piece[0] = new Pawn(Vector2f(0,480),false,sprite,Color::White);
-  piece[1] = new Pawn(Vector2f(80, 480), false, sprite, Color::White);
-  piece[2] = new Pawn(Vector2f(160, 480), false, sprite, Color::White);
-  piece[3] = new Pawn(Vector2f(240, 480), false, sprite, Color::White);
-  piece[4] = new Pawn(Vector2f(320, 480), false, sprite, Color::White);
-  piece[5] = new Pawn(Vector2f(400, 480), false, sprite, Color::White);
-  piece[6] = new Pawn(Vector2f(480, 480), false, sprite, Color::White);
-  piece[7] = new Pawn(Vector2f(560, 480), false, sprite, Color::White);
-  piece[8] = new Rook(Vector2f(0,560),false,sprite,Color::White);
-  piece[9] = new Rook(Vector2f(560, 560), false, sprite, Color::White);
-  piece[10] = new Knight(Vector2f(80, 560), false, sprite, Color::White);
-  piece[11] = new Knight(Vector2f(480, 560), false, sprite, Color::White);
-  piece[12] = new Bishop(Vector2f(160, 560), false, sprite, Color::White);
-  piece[13] = new Bishop(Vector2f(400, 560), false, sprite, Color::White);
-  piece[14] = new King(Vector2f(240, 560), false, sprite, Color::White);
-  piece[15] = new Queen(Vector2f(320, 560), false, sprite, Color::White);
+  pieces[0] = new Pawn(Vector2f(0,480),false,sprite,Color::White);
+  pieces[1] = new Pawn(Vector2f(80, 480), false, sprite, Color::White);
+  pieces[2] = new Pawn(Vector2f(160, 480), false, sprite, Color::White);
+  pieces[3] = new Pawn(Vector2f(240, 480), false, sprite, Color::White);
+  pieces[4] = new Pawn(Vector2f(320, 480), false, sprite, Color::White);
+  pieces[5] = new Pawn(Vector2f(400, 480), false, sprite, Color::White);
+  pieces[6] = new Pawn(Vector2f(480, 480), false, sprite, Color::White);
+  pieces[7] = new Pawn(Vector2f(560, 480), false, sprite, Color::White);
+  pieces[8] = new Rook(Vector2f(0,560),false,sprite,Color::White);
+  pieces[9] = new Rook(Vector2f(560, 560), false, sprite, Color::White);
+  pieces[10] = new Knight(Vector2f(80, 560), false, sprite, Color::White);
+  pieces[11] = new Knight(Vector2f(480, 560), false, sprite, Color::White);
+  pieces[12] = new Bishop(Vector2f(160, 560), false, sprite, Color::White);
+  pieces[13] = new Bishop(Vector2f(400, 560), false, sprite, Color::White);
+  pieces[14] = new King(Vector2f(240, 560), false, sprite, Color::White);
+  pieces[15] = new Queen(Vector2f(320, 560), false, sprite, Color::White);
 }
 void Player::ResetPieces(int i)
 {
@@ -39,7 +40,47 @@ void Player::ResetPieces(int i)
 }
 void Player::PlayTurn()
 {
-  
+  Vector2f previouspos;
+  bool Playturn = false;
+  while(!Playturn)
+  {
+    if(Mouse::isButtonPressed(Mouse::Right))
+    {
+      	Vector2i mousePosition = sf::Mouse::getPosition(window);
+				Vector2f mousePositionFloat = static_cast<sf::Vector2f>(mousePosition);
+				for (int i = 0; i < 16; i++)
+				{
+					int x = (int)mousePositionFloat.x / 80;
+					int y = (int)mousePositionFloat.y / 80;
+					pos = pieces[i]->GetPosition();
+					if (x * 80 == (int)pos.x && (int)pos.y == y * 80)
+					{
+						pieces[i]->SetTargeted(true);
+					}
+					if ((x * 80 != (int)pos.x || (int)pos.y != y * 80) && pieces[i]->GetTargeted() == true)
+					{
+						pieces[i]->SetTargeted(false);
+					}
+				}
+			}
+			if (Mouse::isButtonPressed(Mouse::Left))
+			{
+				Vector2i mousePosition = sf::Mouse::getPosition(window);
+				Vector2f mousePositionFloat = static_cast<sf::Vector2f>(mousePosition);
+				for (int i = 0; i < 16; i++)
+				{
+					if (pieces->GetTargeted() == true)
+					{
+						previouspos = pieces[i]->GetPosition();
+						if (previouspos != pieces[i]->Move(mousePositionFloat))
+						{
+							playerturn = true;
+						}
+					}
+				}
+			}
+    }
+  }
 }
 int Player::GetID()
 {
